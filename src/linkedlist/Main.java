cd5d404127194763e02a5854754b51f31cddd9c7
vinia <viniciusadams98@gmail.com>
2023-07-05 22:28:30 +0100

    changing a bit and adding print method

--- src/linkedlist/Main.java
+++ src/linkedlist/Main.java
@@ -1,68 +1,62 @@
 package linkedlist;
-//How to create linked list,
-//Linked list creation,
-//linked list insertion,
-//linked list creation algorithm
-//linked list creation in java
-//linked list create example
-//linked list Data Structure,
-//coding simplified,
-//linked list,
-//java,
-
-/*
- * Node class contains the LinkedList Node Structure. (int Data & a reference point)
- */
 class Node {
-    Node next;
     int data;
+    Node next;
 }
 
-/*
- * Linked class contains the necessary functions to make the operations.
- */
-class Linked {
-    Node root;
+class LinkedList {
 
-    public Linked() {
-        root = null;
+    private Node getNewNode(int val) {
+        Node newNode = new Node();
+        newNode.data = val;
+        newNode.next =  null;
+
+        return newNode;
     }
 
-    /*
-     * getNewNode() method to generate a new node
-     */
-    public Node getNewNode(int key) {
-        Node a = new Node();
-        a.next = null;
-        a.data = key;
-        return a;
+    public Node insert(Node node, int val) {
+        if(node == null) {
+            return getNewNode(val);
+        }
+
+        Node firstNode = node;
+
+        while(node.next != null) {
+            node = node.next;
+        }
+
+        node.next = getNewNode(val);
+        return firstNode;
     }
 
-    /*
-     * insert method is used to insert the element in Linked List
-     */
-    public Node insert(Node node, int key) {
-        if (node == null)
-            return getNewNode(key);
-        else
-            node.next = insert(node.next, key);
+    public void printList(Node node) {
+        if(node == null) {
+            return;
+        }
 
-        return node;
+        while(node != null) {
+            System.out.print(node.data + " ");
+
+            node = node.next;
+        }
     }
 }
 
-/*
- * LinkedList class to initiate the operation.
- */
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Node root = null;
-        Linked a = new Linked();
-        root = a.insert(root, 1);
-        root = a.insert(root, 2);
-        root = a.insert(root, 3);
-        root = a.insert(root, 4);
+        LinkedList obj = new LinkedList();
+        Node head = null;
+
+        head = obj.insert(head, 12);
+        head = obj.insert(head, 7);
+        head = obj.insert(head, 8);
+        head = obj.insert(head, 10);
+
+        obj.printList(head);
+
     }
+
 }
